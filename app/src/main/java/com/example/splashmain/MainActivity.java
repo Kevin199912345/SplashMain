1b13751c47a7000986f108355df8a8f4b0bca14a
Cristian Corella <cstephexcv@gmail.com>
2019-03-24 20:01:48 -0600

    Implementacion de Firebase

--- app/src/main/java/com/example/splashmain/MainActivity.java
+++ app/src/main/java/com/example/splashmain/MainActivity.java
@@ -4,18 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase fireBD;
+        DatabaseReference refenceBD;
+        FirebaseApp.initializeApp(this);
+/*        fireBD = FirebaseDatabase.getInstance();   */
+       /* refenceBD = fireBD.getReference();*/
+
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
@@ -33,7 +47,30 @@ public class MainActivity extends AppCompatActivity {
         adapter Adapter = new adapter(this,mlist);
         recyclerView.setAdapter(Adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//Hola Kevin 111111
-        //Que pasa!!!
+
+       /* //Mensaje para la base de datos(firebase)
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+        //---------------------------------------
+
+        //Leer desde la base de datos
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Failed to read value.", error.toException());
+            }
+        });*/
     }
+
 }
